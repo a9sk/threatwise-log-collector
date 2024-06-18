7bb9d93d2d466ed2a6f19e1ad47303ce8e01c6b8
a9sk <167626445+a9sk@users.noreply.github.com>
2024-06-18 10:05:50 +0200

    beautify

--- scripts/winnie.py
+++ scripts/winnie.py
@@ -5,32 +5,45 @@ import json
 import configparser
 import json
 
-# variabili globali
+# -------------------
+# Global Variables
+
 traps_data = ""
 
 def initial_setup(filename):
-    # file ini per la configurazione contenente tutte le robe che servono
+    # -----NEEDS-----
+    # File ini for the configuration
 
-    # lista di dizionari per tenere la configurazione
+    # -----USES-----
+    # List of dicts to keep the trap's configurations 
+
     config = configparser.ConfigParser()
     config.read(filename)
 
     traps = []
 
+    # -------------------
+    # Iterate through the config file and load it  
+
     for section in config.sections():
         trap = {} 
         TEMP = json.loads(config[section]["filter"])
         trap['name'] = f"{TEMP.get('trap_name')}-{TEMP.get('department')}"
-        #print(f"[debug] name: {trap['name']}")
         trap['modified_address'] = generate_address(config[section]['url'], config[section]['version'])
-        #print(f"[debug] address: {trap['modified_address']}")
         trap['version'] = config[section]['version']
-        #print(f"[debug] version: {trap['version']}")
         trap['api_key'] = config[section]['key']
-        #print(f"[debug] api key: {trap['api_key']}")
         trap['payload'] = json.loads(config[section]['filter'].replace("'", '"'))
-        #print(f"[debug] payload: {trap['search_payload']}")
-        print(trap)
+
+        #  -------DEBUG------
+        # Commands usefull for debugging
+
+        # print(f"[debug] Name: {trap['name']}")
+        # print(f"[debug] Address: {trap['modified_address']}")
+        # print(f"[debug] Payload: {trap['search_payload']}")
+        # print(f"[debug] Version: {trap['version']}")
+        # print(f"[debug] Api Key: {trap['api_key']}")
+        # print(f"[debug] Trap: {trap}")
+
         traps.append(trap)
 
     return traps
@@ -45,28 +58,47 @@ Use the flag -f to insert the .ini configuration file.
     exit()
     
 def generate_address(url, version):
-    # prendo la versione della trappola
+    # -------------------
+    # The API uses port 8443 for https API communication, you might want to add some policy for that on your firewall 
+
     parts = url.split('.')
     mod_address = f"{parts[0]}-apl.threatwise.metallic.io:8443/api/v{version}"
     return mod_address
 
 def write_logs(logs, filename):
-    # salvo tutto in un file json, questa parte penso che vada cambiata
+    # -------------------
+    # Save everything in a json file
+
     with open(filename, 'a') as file:
         timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
         file.write(f'"time":"{timestamp}"\n {json.dumps(logs)}\n')
 
 def make_post_request(url, payload):
+    # -------------------
+    # Post requests are all made from this same function
+
     response = requests.post(url, json=payload)
-    # print(f"{response.text}\n\n\n")
+
+    #  -------DEBUG------
+    # Commands usefull for debugging
+
+    # print(f"[debug] Response: {response.text}\n")
+
     return response
 
 def save_file(file, filename):
-    # il file si salva con url_data e ora.estensione
+    # -------------------
+    # The file is saved using the timestamp and some other characteristics to make it unique
+
     timestamp = time.strftime("%Y-%m-%d_%H-%M-%S")
     parts= filename.split('.')
     filename = f"{parts[0]}_{timestamp}.{parts[1]}"
-    # print(file.text)
+
+    #  -------DEBUG------
+    # Commands usefull for debugging    
+
+    # print(f"[debug] Content: {file.text}")
+
     with open(filename, 'wb') as f:
         f.write(file.text.encode('Utf-8'))
 
@@ -76,18 +108,33 @@ def save_logs(trap):
         "api_key":f"{trap['api_key']}",
         "filter": trap['payload']
     }
-    # print(search_payload)
 
-    # mi prendo i valori dalla singola trappola
+    #  -------DEBUG------
+    # Commands usefull for debugging
+    
+    # print(f"[debug] Search Payload: {search_payload})
+
+    # -------------------
+    # This takes the values for a sigle trap per iteration
+
     search_url = f"{trap['modified_address']}/events/search"
 
     search_response = make_post_request(search_url, search_payload).json()
 
     search_id = search_response.get("search_id")
-    # print(search_id)
+
+    #  -------DEBUG------
+    # Commands usefull for debugging
+
+    # print(f"[debug] Search ID: {search_id})
+
     if search_id:
         events=[]
         pages = search_response.get("number_of_pages")
+
+        # -------------------
+        # If the response has multiple pages we will need to iterate throught all of them
+
         if pages:
             for page in pages:
                 show_payload = {
@@ -112,12 +159,26 @@ def save_logs(trap):
 
             events.extend(show_response.get("events", []))
     
+        # -------------------
+        # Save the logs always in the same file (every trap should have its own)
+
         write_logs(events, f"{trap['name']}-logs.json")
 
+        # -------------------
+        # Iterate trought the events in the response and download eventual files (the downloads are disabled, to enable them remove the # sign)
+
         for event in events:
-            # Se la risposta alla search contiene file li scarico e li salvo
+
             if event.get("x_trapx_com_pcap") == True:
-                print(f"[!] Looks like there is a .pcap file in the event with id: {event.get('x_trapx_com_eventid')}")
+                
+                #  -------DEBUG------
+                # Commands usefull for debugging    
+
+                print(f"[debug] Looks like there is a .pcap file in the event with id: {event.get('x_trapx_com_eventid')}")
+
+                # -------------------
+                # Actual code for downloading the file
+
                 # download_url = f"{trap['modified_address']}/events/download"
                 # download_payload = {
                 #     "api_key": trap['api_key'],
@@ -128,7 +189,15 @@ def save_logs(trap):
                 # save_file(content, f"{trap['name']}_{event.get('x_trapx_com_eventid')}.pcap")
         
             if event.get("x_trapx_com_binary") == True:
+                                
+                #  -------DEBUG------
+                # Commands usefull for debugging  
+
                 print(f"[!] Looks like there is a binary file in the event with id: {event.get('x_trapx_com_eventid')}")
+
+                # -------------------
+                # Actual code for downloading the file
+
                 # download_url = f"{trap['modified_address']}/events/download"
                 # download_payload = {
                 #     "api_key": trap['api_key'],
@@ -138,15 +207,18 @@ def save_logs(trap):
                 # content=  make_post_request(download_url, download_payload)
                 # save_file(content, f"{trap['name']}_{event.get('x_trapx_com_eventid')}.zip")
 
+        # -------------------
+        # Cancel logs from the TSOC's cache (default is commented, you might want to enable this)
+
         # cancel_payload = {
         # "api_key":f"{trap['api_key']}",
         # "search_id": search_id
         # }
         # cancel_url = f"{trap['modified_address']}/events/cancel"
         # cancel_response = make_post_request(cancel_url, cancel_payload)
-        # print(cancel_response)
         
-        # deletando tutti i log si è sicuri che vengan loggati solo gli eventi nuovi
+        # -------------------
+        # Delete logs so that they are not repeated in the next scan (do not disable this)
 
         delete_payload = {
         "api_key":f"{trap['api_key']}",
@@ -156,6 +228,9 @@ def save_logs(trap):
         make_post_request(delete_url, delete_payload)
 
     else:
+        #  -------DEBUG------
+        # Commands usefull for debugging
+
         print("[!] No search_id was found in the response")
 
 def main(traps):
@@ -175,7 +250,10 @@ if __name__ == "__main__":
         filename=args[1]
     try:
         traps_data = initial_setup(filename)
-        # print(traps_data)
+        #  -------DEBUG------
+        # Commands usefull for debugging
+
+        # print(f"[debug] {traps_data}")
     except KeyboardInterrupt:
         print("[!] Keyboad Interrupt detected, exiting...")
         exit
@@ -183,9 +261,11 @@ if __name__ == "__main__":
     while True:
         try:
             main(traps_data)
-            # fa un check ogni 10 minuti
+            # -------------------
+            # Does a check every x time (defaut 600 seconds)
+            
             print("[...] Waiting...")
-            time.sleep(60)
+            time.sleep(600)
         except KeyboardInterrupt:
             print("[!] Keyboad Interrupt detected, exiting...")
             exit()
